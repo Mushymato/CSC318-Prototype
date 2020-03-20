7720f9a3bd0893356ed82b077dc1c07b38b2df97
mushymato <wildshinobu@gmail.com>
2020-03-20 14:42:35 -0400

    search

--- prototype.js
+++ prototype.js
@@ -1,3 +1,27 @@
+const data = {
+    HISA: {
+        name: 'High Interest Savings Account',
+        definition: 'High interest savings accounts, as their name suggests, offer a much higher interest rate than what’s available with a regular savings account. Financial institutions are able to offer these rates for a few different reasons:\n<ul>\n<li>Some of the banks are branchless, which cuts down on costs.</li>\n<li>Because transactions can be costly, most customers don’t use these accounts for daily activity, so from the bank’s end there aren’t many transactions to process (which also keeps costs low).</li>\n<li>Banks offer high interest rates, in order to acquire new customers.</li>\n</ul>',
+        learn: [
+            {
+                type: 'video',
+                link: 'https://www.youtube.com/embed/8edPzh71RIQ'
+            },
+            {
+                type: 'reading',
+                link: 'https://en.wikipedia.org/wiki/Interest'
+            }
+        ],
+        practice: [
+            {
+                type: 'question',
+                question: 'How much interest do high interest saving accounts typically offer?',
+                choices: ['0.5%', '2.0%', '5.0%', '10.0%']
+            }
+        ]
+    }
+}
+
 function getParams(url) {
     var params = {};
     var parser = document.createElement('a');
